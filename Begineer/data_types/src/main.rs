3c46d8621213e0408f69321c86585e6d02494329
ashdotenv <ashdotenv@gmail.com>
2024-08-07 15:04:03 +0545

    Init

---
+++ Begineer/data_types/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // Boolean
+    let a = true;
+    let b = false;
+    // unsigned Intergers #can't be negative
+    let x: u32 = 50;
+    let y: u8 = 50;
+    // signed Intergers #can be negative
+    let z: i32 = -40;
+    // arrays
+    let mut a: [i32; 5] = [1, 2, 4, 5, 6];
+    a[0] = 5;
+    //type aliasing
+    type age = i32;
+    let m: age = 50;
+}
