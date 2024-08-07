a5bc7d8d15e5786eb8f6ba1720e34387727c1654
ashdotenv <ashdotenv@gmail.com>
2024-08-07 15:11:06 +0545

    Init

---
+++ Begineer/static_and_constants/src/main.rs
@@ -0,0 +1,7 @@
+const PLAYER_HEIGHT: u32 = 170;
+static PLAYER_AGE: u32 = 20;
+fn main() {
+    // const ants dont occupy a specific memory while static do occupy
+    let a = PLAYER_AGE;
+    let b = PLAYER_HEIGHT;
+}
